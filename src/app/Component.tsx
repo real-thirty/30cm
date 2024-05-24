dc54e38743d93b45425c9e03a10153639ff7b7a6
장한울 <swingjhw@naver.com>
2024-05-25 00:57:32 +0900

    feat: react-query 설치

---
+++ src/app/Component.tsx
@@ -0,0 +1,5 @@
+function Component() {
+  return <div>test</div>;
+}
+
+export default Component;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const queryClient = new QueryClient();
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <QueryClientProvider client={queryClient}>
+        <body className={inter.className}>{children}</body>
+      </QueryClientProvider>
     </html>
   );
 }
