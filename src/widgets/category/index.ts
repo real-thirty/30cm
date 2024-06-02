9263ba2ff6849b0e06b1679baaa6c7acde8683ba
장한울 <swingjhw@naver.com>
2024-06-03 03:07:36 +0900

    fix: conflict solved 5

--- src/widgets/category/index.ts
+++ src/widgets/category/index.ts
@@ -1,6 +1,2 @@
 export {CategoryLayout} from './ui/CategoryLayout'
-<<<<<<< HEAD
 export {ProductListInCategoryLayout} from './ui/ProductListInCategoryLayout'
-=======
-export {ProductListInCategoryLayout} from './ui/ProductListInCategoryLayout'
->>>>>>> 77c459d (refactor: CategorySortBySiderbar를 widgets에서 features로 이동)
