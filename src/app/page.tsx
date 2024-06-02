283b5143167064f8ebeb070a382c3775968a673c
장한울 <swingjhw@naver.com>
2024-06-03 03:49:49 +0900

    fix: conflict solved 14

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { Footer, Header } from "@/shared/ui";
 const { Content } = Layout;
 
 export default function Home() {
+  const { data, isSuccess } = useProductListQuery();
   return (
     <main>
       <Layout>
