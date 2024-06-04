83129db855800c084b701d8649690c6b0685d667
장한울 <swingjhw@naver.com>
2024-06-04 14:18:30 +0900

    feat: queryKey 따로 분리

--- src/entities/product/constants/queryKey.ts
+++ src/entities/product/constants/queryKey.ts
@@ -3,5 +3,6 @@ const PRODUCT = "@product";
 export const productQueryKey = {
   product: [PRODUCT],
   getProduct: (id: string) => [PRODUCT, id],
+  getProductList: (from:number, to:number) => [PRODUCT, from, to]
 };
 
--- src/entities/product/hooks/queries/use-product-list-query.ts
+++ src/entities/product/hooks/queries/use-product-list-query.ts
@@ -9,7 +9,7 @@ export const useProductListQuery = (from:number, to:number) => {
   const supabase = useSupabaseBrowser();
 
   return useQuery({
-    queryKey: [productQueryKey.product, from , to],
+    queryKey:  productQueryKey.getProductList(from, to),
     queryFn: () => ProductService.getProductList(supabase, from, to),
   });
 };
